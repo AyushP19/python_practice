328ba945550a944d87018f665b8cc64d6890cd3b
AyushP19 <ayushpandaey.ayush7878@gmail.com>
2024-09-19 19:41:10 +0530

    METHOD TO SOLVE IN LESS LINE

--- Assign1/q1.py
+++ Assign1/q1.py
@@ -1,11 +1,16 @@
-i=1
-while i<=3:
-    n = int(input("enter no."))
-    if(n%2==0):
-     print("WINNER") 
-     i=i+1 
-    else: 
-       print("LOOSE") 
+# i=1
+# whi le i<=3:
+#     n = int(input("enter no."))
+#     if(n%2==0):
+#      print("WINNER") 
+#      i=i+1 
+#     else: 
+#        print("LOOSE") 
 
 # if(i==4):
-    # print("LOOSE")   
+    # print("LOOSE")  
+
+#METHOD TO SOLVE IN LESS LINE
+s2="1a2b3c4"
+a=sum([ int(e) for e in s2 if ord(e)>=49  and ord(e)<57])
+print(a)
